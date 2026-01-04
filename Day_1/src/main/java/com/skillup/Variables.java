e3629d3d347cef2c26d24e6c1fefe51e4a9ecc5b
SupunVirajDevendra <zerohreo01@gmail.com>
2026-01-05 00:26:23 +0530

    intial commit

--- Day_1/src/main/java/com/skillup/Variables.java
+++ Day_1/src/main/java/com/skillup/Variables.java
@@ -7,11 +7,20 @@ public class Variables {
         char myLetter = 'D';         // character
         boolean myBool = true;       // boolean
         String myText = "Hello";    // String
+
         System.out.println(" ");
         System.out.println(myNum);
         System.out.println(myFloatNum);
         System.out.println(myLetter);
         System.out.println(myBool);
         System.out.println(myText);
+
+        /*In Java, there are different types of variables, for example:
+
+        String - stores text, such as "Hello". String values are surrounded by double quotes
+        int - stores integers (whole numbers), without decimals, such as 123 or -123
+        float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        boolean - stores values with two states: true or false*/
     }
 }
